2589b52ff5e6e695fe02094e661b67a5a6718a82
Alilya <alinakolesnikova0101@gmail.com>
2023-04-02 18:21:09 +0300

    Значения из формы

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 document.forms[0].addEventListener("submit", (e) => {
   e.preventDefault();
   let inputs = e.target.elements;
- 
-  alert(inputs.age.value);
-  alert(inputs.phone.value);
+  alert(
+    "Ваше Имя: " +
+      inputs.Name.value +
+      "\n" +
+      "Ваш возраст: " +
+      inputs.age.value +
+      "\n" +
+      "Номер телефона: " +
+      inputs.phone.value
+  );
+  let checkboxes = document.querySelectorAll("input:checked");
+  let values = [];
+  checkboxes.forEach((checkbox) => {
+    values.push(checkbox.value);
+  });
+  alert("Направления: " + values);
+  e.target.reset();
 });
